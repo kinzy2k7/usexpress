d7667bc2ec39e43b4ce225ee55d96ad96e0eb61e
kinzy2k7 <nql08062007@gmail.com>
2026-03-15 16:43:48 +0700

    qc2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7796825527400019"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         <Suspense fallback={null}>
           <GoogleAnalytics />
